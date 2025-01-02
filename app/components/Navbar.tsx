a67baa1cfed50adab8b7a6a97a99297d304686b7
Ethan Gibbs <emg2194@gmail.com>
2025-01-01 23:59:05 -0500

    improved modularity of front page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+export default function Navbar() {
+    return (
+        <nav className="sticky top-0 w-screen flex justify-between items-center py-6 px-4 mb-6 bg-black bg-opacity-85 z-10">
+            {/* Left Side: Logo */}
+            <Link href={"#home"}>
+                <h1 className="text-xl font-bold text-white font-funnel">Ethan Gibbs</h1>
+            </Link>
+            {/* Right Side: Icons */}
+            <div className="flex space-x-4 font-funnel">
+                <Link
+                    href="https://www.linkedin.com/in/etgibbs/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-300 transition"
+                >
+                    <FaLinkedin className="text-2xl" />
+                </Link>
+                <Link
+                    href="https://github.com/etgibbs"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-300 transition"
+                >
+                    <FaGithub className="text-2xl" />
+                </Link>
+            </div>
+        </nav>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import React from "react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Ethan Gibbs",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="root scroll-smooth">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
